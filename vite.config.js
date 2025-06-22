54b3f850972efc1d70eb12915c591fac816af895
hung <ff789456741@gmail.com>
2025-06-23 02:20:47 +0800

    add: vite base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,7 @@ export default defineConfig({
       }
     }
   })],
+  base: '/carrywell-vue/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
